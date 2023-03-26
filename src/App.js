6106d3cd4b688db62b85072cd8c9e72ce6b4d749
0JIEUN0 <je300512@gmail.com>
2023-03-26 17:44:23 +0900

    feat(router): add main router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  },
+]);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello
-        </p>
-      </header>
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
