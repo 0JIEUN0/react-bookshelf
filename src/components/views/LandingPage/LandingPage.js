931f2f4cb03871c99c4eae05fe81d6188aa8aa66
0JIEUN0 <je300512@gmail.com>
2023-04-01 16:56:45 +0900

    feat(user): add support for logout

--- src/components/views/LandingPage/LandingPage.js
+++ src/components/views/LandingPage/LandingPage.js
@@ -1,12 +1,23 @@
+import axios from 'axios'
 import React, { useEffect } from 'react'
 
 function LandingPage() {
+
+  const onClickHandler = () => {
+    axios.get('/logout')
+      .then(response => console.log(response));
+  }
+
   return (
     <div style={{
       display: 'flex', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '100vh',
     }}>
       <h2>LandingPage</h2>
+      
+      <button onClick={onClickHandler}>
+        Logout
+      </button>
     </div>
   )
 }
