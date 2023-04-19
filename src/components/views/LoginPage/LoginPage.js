14a91e6e952adf142c0be4e82feee2afa7f27fb5
0JIEUN0 <je300512@gmail.com>
2023-04-20 01:37:43 +0900

    feat(login): apply UI and add register button

--- src/components/views/LoginPage/LoginPage.js
+++ src/components/views/LoginPage/LoginPage.js
@@ -2,50 +2,61 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../_actions/user_action';
 import { useNavigate } from 'react-router-dom';
+import './LoginPage.css';
 
 function LoginPage() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
-  const onSubmitHandler = (event) => {
-    event.preventDefault();
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
 
-    let body = {
-      email: email,
-      password: password
-    }
-    
-    dispatch(loginUser(body))
-      .then(response => {
-        console.log(response.payload)
-        if (response.payload.success) {
-          navigate('/library');
-        } else {
-          alert('Fail: Please check your email or password.')
+        let body = {
+            email: email,
+            password: password
         }
-      })
-  }
 
-  return (
-    <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center',
-      width: '100%', height: '100vh',
-    }}>
-      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
-        <label>Email</label>
-        <input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
-        <label>Password</label>
-        <input type='password' value={password} onChange={(e)=>setPassword(e.target.value)} />
-        <br />
-        <button type='submit'>
-          Login
-        </button>
-      </form>
-    </div>
-  )
+        dispatch(loginUser(body))
+            .then(response => {
+                console.log(response.payload)
+                if (response.payload.success) {
+                    navigate('/library');
+                } else {
+                    alert('Fail: Please check your email or password.')
+                }
+            })
+    }
+
+    return (
+        <div className='wrap'>
+            <img src="/image/handshelf.png" width="15%" />
+
+            <div className='login'>
+                <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
+                    <div className='login_email'>
+                        <h4>Email</h4>
+                        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+
+                    <div className='login_pw'>
+                        <h4>Password</h4>
+                        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+
+                    <button className='loginBtn' type='submit'>
+                        Login
+                    </button>
+                </form>
+            </div>
+
+            <div className="register">
+                <a href="/register">💡 Register Now</a>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
